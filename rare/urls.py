f7580d9951e9ac8ac0573fb8f41989a561298331
Jubjub422 <ross.stovenour@gmail.com>
2022-02-14 14:20:46 -0600

    added categoryviews and other changes to allow for client side to get categories

--- rare/urls.py
+++ rare/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rareapi.views import register_user, login_user
+from rest_framework import routers
+from rareapi.views import CategoryView
+
 # from rest_framework import routers
+router=routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
