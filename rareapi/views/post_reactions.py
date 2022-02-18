22495777af02f416952ddbf91c968cadb43117e7
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-18 13:12:28 -0600

    deploying

--- rareapi/views/post_reactions.py
+++ rareapi/views/post_reactions.py
@@ -12,32 +12,12 @@ class PostReactionView(ViewSet):
         serializer = PostReactionSerializer(postReactions, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, pk):
-        """handles the GET for single Postreactions"""
-        try:
-            postReactions = PostReaction.objects.get(pk=pk)
-            serializer = PostReactionSerializer(postReactions)
-            return Response(serializer.data)
-        except PostReaction.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-
     def create(self, request):
-        
         try:
             serializer = CreatePostReactionSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except ValidationError as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk):
-        try:
-            postReactions = PostReaction.objects.get(pk=pk)
-            serializer = CreatePostReactionSerializer(postReactions, data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(None, status=status.HTTP_204_NO_CONTENT)
         except ValidationError as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
