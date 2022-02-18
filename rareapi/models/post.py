71cf8e7b7614ed5e651e0c7135c232f77388b33f
jmason87 <joewebdev87@gmail.com>
2022-02-18 09:03:42 -0600

    commit for merge

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -8,7 +8,9 @@ class Post(models.Model):
     category = models.ForeignKey("Category", on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=100)
     publication_date = models.DateField(auto_now_add=True)
-    image_url = models.URLField()
+    image_url = models.ImageField(
+        upload_to='postpics', height_field=None,
+        width_field=None, max_length=None, null=True)
     content = models.TextField()
     approved = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", through="PostTag", related_name="tags")
