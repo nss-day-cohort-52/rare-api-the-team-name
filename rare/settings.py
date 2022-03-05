60da8669b0b6edfc64cdfebed19859a15a922ae4
jmason87 <joewebdev87@gmail.com>
2022-03-05 13:32:27 -0600

    added heroku whitelist

--- rare/settings.py
+++ rare/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
     'https://rare-server.herokuapp.com'
 )
 
