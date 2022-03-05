d7eb0caa9229e686f254373d80dcbc6f3c251bcb
jmason87 <joewebdev87@gmail.com>
2022-03-05 11:59:11 -0600

    attempt to deploy #1

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -4,11 +4,11 @@ from django.db import models
 class Post(models.Model):
     user = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.DO_NOTHING)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     publication_date = models.DateField(auto_now_add=True)
     image_url = models.ImageField(
         upload_to='postpics', height_field=None,
-        width_field=None, max_length=None, null=True)
-    content = models.TextField()
+        width_field=None, max_length=1000, null=True)
+    content = models.TextField(max_length=1000)
     approved = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", through="PostTag", related_name="tags")
